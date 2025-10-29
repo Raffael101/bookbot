23e68a4928667bbd1631c4f6a52f49de76bfc86f
Raffael101 <239906373+Raffael101@users.noreply.github.com>
2025-10-29 10:38:20 +0800

    7: Day 1?

--- stats.py
+++ stats.py
@@ -1,6 +1,13 @@
 
-def letter_count():
-    
+def letter_count(title):
+    book = read_book(title)
+    lower_book = book.lower
+    alphabet = {}
+    if alphabet in lower_book:
+        alphabet[i] += 1
+    else:
+        alphabet[i] += 1
+    print(alphabet)
     pass
 
 def word_count(title):
@@ -12,3 +19,5 @@ def read_book(title):
     with open(f"books/{title}.txt") as f:
         book = f.read()
     return book
+
+letter_count("frankenstein")
