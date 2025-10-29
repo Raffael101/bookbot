0d3ce9d204b1876a6c72963552020a2382ced4b6
Raffael101 <239906373+Raffael101@users.noreply.github.com>
2025-10-29 09:57:17 +0800

    2: add functions read book and word count on main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def read_book(title):
+    with open(f"books/{title}.txt") as f:
+        book = f.read()
+    return book
+
+
+def word_count(title):
+    book = read_book(title)
+    word = book.split() 
+    print(f"Found {len(word)} total words")
+
+
+def main():
+    title = input("Which Book?\n")
+    #read_book(title)
+    word_count(title)
+
+main()
