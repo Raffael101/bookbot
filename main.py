d32a91bd6a614e88c4326e466d8adeb5467c2fd9
Raffael101 <239906373+Raffael101@users.noreply.github.com>
2025-10-29 10:04:15 +0800

    4: add stats.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
+from stats import word_count
+
 def read_book(title):
     with open(f"books/{title}.txt") as f:
         book = f.read()
     return book
-
-
-def word_count(title):
-    book = read_book(title)
-    word = book.split() 
-    print(f"Found {len(word)} total words")
-
 
 def main():
     title = "frankenstein"
